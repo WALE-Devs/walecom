771f33a20b91659d417e2929e4ca007b86d2aab5
Walter Capa <walterc316@gmail.com>
2026-01-27 00:09:35 -0500

    upd: Improved unique_slugify. Now it does not have a caveman N+1 performance

--- src/utils/slug_utils.py
+++ src/utils/slug_utils.py
@@ -1,11 +1,16 @@
+import secrets
+import string
 from django.utils.text import slugify
 
+def generate_nonce(length=4):
+    """Generates a short, URL-friendly random string."""
+    alphabet = string.ascii_lowercase + string.digits
+    return ''.join(secrets.choice(alphabet) for _ in range(length))
+
 def unique_slugify(instance, value, slug_field_name='slug'):
-    slug = slugify(value)
-    ModelClass = instance.__class__
-    unique_slug = slug
-    counter = 1
-    while ModelClass.objects.filter(**{slug_field_name: unique_slug}).exists():
-        unique_slug = f"{slug}-{counter}"
-        counter += 1
-    return unique_slug
+    """
+    Generates a unique slug by appending a random nonce.
+    This avoids expensive database lookups and race conditions.
+    """
+    base_slug = slugify(value)
+    return f"{base_slug}-{generate_nonce()}"
